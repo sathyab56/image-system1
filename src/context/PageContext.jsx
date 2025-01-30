5094732b6a4e4e18638112bcdb8e17108b5d6768
sathyab56 <147052015+sathyab56@users.noreply.github.com>
2025-01-30 22:27:36 +0530

    Update PageContext.jsx

--- src/context/PageContext.jsx
+++ src/context/PageContext.jsx
@@ -11,7 +11,7 @@ const PageContextProvider = (props) => {
     const currency = "₹"
 
     //const backendUrl = import.meta.env.VITE_BACKEND_URL
-    const backendUrl = "https://image-system-backend.onrender.com"
+    const backendUrl = "http://localhost:4000"
     
 
     const navigate = useNavigate();
