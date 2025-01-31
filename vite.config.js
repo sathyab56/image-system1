2e4daf7af65bcd7cf1fffde791505e3e2095b2ae
sathyab56 <sathyasubramaniam80@gmail.com>
2025-01-31 15:00:59 +0530

    god

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/", // Ensures correct routing on Vercel
   server: {
     proxy: {
       "/api": {
